d311ed48ad758a48fc687db96b1ad5b2815cfeb6
Salim Afiune Maya <afiune@lacework.net>
2020-04-29 23:00:57 -0600

    refactor(cli): new configure command using survey
    Signed-off-by: Salim Afiune Maya <afiune@lacework.net>

--- cli/cmd/configure.go
+++ cli/cmd/configure.go
@@ -20,12 +20,11 @@ package cmd
 
 import (
 	"bytes"
-	"fmt"
 	"io/ioutil"
 	"path"
 
+	"github.com/AlecAivazis/survey/v2"
 	"github.com/BurntSushi/toml"
-	"github.com/manifoldco/promptui"
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
@@ -49,8 +48,8 @@ type Profiles map[string]credsDetails
 
 type credsDetails struct {
 	Account   string `toml:"account" json:"account"`
-	ApiKey    string `toml:"api_key" json:"api_key"`
-	ApiSecret string `toml:"api_secret" json:"api_secret"`
+	ApiKey    string `toml:"api_key" json:"api_key" survey:"api_key"`
+	ApiSecret string `toml:"api_secret" json:"api_secret" survey:"api_secret"`
 }
 
 var (
@@ -72,109 +71,116 @@ You can configure multiple profiles by using the --profile argument. If a
 config file does not exist (the default location is ~/.lacework.toml), the
 Lacework CLI will create it for you.`,
 		RunE: func(_ *cobra.Command, _ []string) error {
-			cli.Log.Debugw("configuring cli", "profile", cli.Profile)
-			var (
-				promptAccount = promptui.Prompt{
-					Label:   "Account",
-					Default: cli.Account,
-					Validate: func(input string) error {
-						if len(input) == 0 {
-							return errors.New(
-								"The account subdomain of URL is required. (i.e. <ACCOUNT>.lacework.net)")
-						}
-						return nil
-					},
-				}
-				promptApiKey = promptui.Prompt{
-					Label:   "Access Key ID",
-					Default: cli.KeyID,
-					Validate: func(input string) error {
-						if len(input) < 55 {
-							return errors.New(
-								"The API access key id must have more than 55 characters.")
-						}
-						return nil
-					},
-				}
-				promptApiSecret = promptui.Prompt{
-					Label:   "Secret Access Key",
-					Default: cli.Secret,
-					Validate: func(input string) error {
-						if len(input) < 30 {
-							return errors.New(
-								"The API secret access key must have more than 30 characters.")
-						}
-						return nil
-					},
-				}
-			)
-			account, err := promptAccount.Run()
-			if err != nil {
-				return err
-			}
-			apiKey, err := promptApiKey.Run()
-			if err != nil {
-				return err
-			}
-			apiSecret, err := promptApiSecret.Run()
-			if err != nil {
-				return err
-			}
-
-			var (
-				profiles = Profiles{}
-				buf      = new(bytes.Buffer)
-				confPath = viper.ConfigFileUsed()
-			)
-
-			if confPath == "" {
-				home, err := homedir.Dir()
-				if err != nil {
-					return err
-				}
-				confPath = path.Join(home, ".lacework.toml")
-				cli.Log.Debugw("generating new config file",
-					"path", confPath,
-				)
-			} else {
-				if _, err := toml.DecodeFile(confPath, &profiles); err != nil {
-					cli.Log.Debugw("unable to decode profiles from config, trying previous config",
-						"path", confPath, "error", err,
-					)
-
-					var oldcreds credsDetails
-					if _, err2 := toml.DecodeFile(confPath, &oldcreds); err2 != nil {
-						cli.Log.Debugw("unable to decode old config, no more options, exit",
-							"error", err2,
-						)
-						return err
-					}
-					profiles["default"] = oldcreds
-				}
-				cli.Log.Debugw("profiles loaded from config, updating", "profiles", profiles)
-			}
-
-			profiles[cli.Profile] = credsDetails{
-				Account:   account,
-				ApiKey:    apiKey,
-				ApiSecret: apiSecret,
-			}
-
-			cli.Log.Debugw("storing updated profiles", "profiles", profiles)
-			if err := toml.NewEncoder(buf).Encode(profiles); err != nil {
-				return err
-			}
-
-			err = ioutil.WriteFile(confPath, buf.Bytes(), 0600)
-			if err != nil {
-				return err
-			}
-			fmt.Println("\nYou are all set!")
-			return nil
+			return promptConfigureSetup()
 		},
 	}
 )
 
 func init() {
 	rootCmd.AddCommand(configureCmd)
+}
+
+func promptConfigureSetup() error {
+	cli.Log.Debugw("configuring cli", "profile", cli.Profile)
+	questions := []*survey.Question{
+		{
+			Name: "account",
+			Prompt: &survey.Input{
+				Message: "Account: ",
+				Default: cli.Account,
+			},
+			Validate: func(input interface{}) error {
+				if str, ok := input.(string); !ok || len(str) == 0 {
+					return errors.New(
+						"The account subdomain of URL is required. (i.e. <ACCOUNT>.lacework.net)")
+				}
+				return nil
+			},
+		},
+		{
+			Name: "api_key",
+			Prompt: &survey.Input{
+				Message: "Access Key ID: ",
+				Default: cli.KeyID,
+			},
+			Validate: func(input interface{}) error {
+				if str, ok := input.(string); !ok || len(str) < 55 {
+					return errors.New(
+						"The API access key id must have more than 55 characters.")
+				}
+				return nil
+			},
+		},
+		{
+			Name: "api_secret",
+			Prompt: &survey.Input{
+				Message: "Secret Access Key: ",
+				Default: cli.Secret,
+			},
+			Validate: func(input interface{}) error {
+				if str, ok := input.(string); !ok || len(str) < 30 {
+					return errors.New(
+						"The API secret access key must have more than 30 characters.")
+				}
+				return nil
+			},
+		},
+	}
+
+	newCreds := credsDetails{}
+	err := survey.Ask(questions, &newCreds,
+		survey.WithIcons(func(icons *survey.IconSet) {
+			icons.Question.Text = "▸"
+		}),
+	)
+	if err != nil {
+		return err
+	}
+
+	var (
+		profiles = Profiles{}
+		buf      = new(bytes.Buffer)
+		confPath = viper.ConfigFileUsed()
+	)
+
+	if confPath == "" {
+		home, err := homedir.Dir()
+		if err != nil {
+			return err
+		}
+		confPath = path.Join(home, ".lacework.toml")
+		cli.Log.Debugw("generating new config file",
+			"path", confPath,
+		)
+	} else {
+		if _, err := toml.DecodeFile(confPath, &profiles); err != nil {
+			cli.Log.Debugw("unable to decode profiles from config, trying previous config",
+				"path", confPath, "error", err,
+			)
+
+			var oldcreds credsDetails
+			if _, err2 := toml.DecodeFile(confPath, &oldcreds); err2 != nil {
+				cli.Log.Debugw("unable to decode old config, no more options, exit",
+					"error", err2,
+				)
+				return err
+			}
+			profiles["default"] = oldcreds
+		}
+		cli.Log.Debugw("profiles loaded from config, updating", "profiles", profiles)
+	}
+
+	profiles[cli.Profile] = newCreds
+	cli.Log.Debugw("storing updated profiles", "profiles", profiles)
+	if err := toml.NewEncoder(buf).Encode(profiles); err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(confPath, buf.Bytes(), 0600)
+	if err != nil {
+		return err
+	}
+
+	cli.OutputHuman("\nYou are all set!\n")
+	return nil
 }
