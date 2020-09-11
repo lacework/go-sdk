403a29f57a87bbda64f52f967171f9030510cff6
Salim Afiune Maya <afiune@lacework.net>
2020-09-11 17:39:59 +0200

    feat(ctr): use new lacework/lacework-cli repository
    Signed-off-by: Salim Afiune Maya <afiune@lacework.net>

--- scripts/release_containers.sh
+++ scripts/release_containers.sh
@@ -4,8 +4,9 @@
 set -eou pipefail
 
 # The repository where we are hosting the lacework-cli containers
-# TODO @afiune switch it to "lacework/lacework-cli" repository
-readonly repository="techallylw/lacework-cli"
+readonly repository="lacework/lacework-cli"
+# @afiune let us continue posting the Lacework CLI to the old reporitory for a few
+readonly old_repo="techallylw/lacework-cli"
 readonly project_name=lacework-cli
 
 log() {
@@ -30,6 +31,11 @@ log "releasing container from SCRATCH"
 docker build -t "${repository}:scratch" --no-cache .
 docker push "${repository}:scratch"
 
+# @afiune let us continue posting the Lacework CLI to the old reporitory for a few
+log "releasing old container for SCRATCH"
+docker image tag "${repository}:scratch" "${old_repo}:scratch"
+docker push "${old_repo}:scratch"
+
 # when updating the distributions below, please make sure to update
 # the script 'release.sh' inside the 'script/' folder
 distros=(
@@ -45,6 +51,11 @@ for dist in "${distros[@]}"; do
   log "releasing container for ${dist}"
   docker build -f "cli/images/${dist}/Dockerfile" --no-cache -t "${repository}:${dist}" .
   docker push "${repository}:${dist}"
+
+  # @afiune let us continue posting the Lacework CLI to the old reporitory for a few
+  log "releasing old container for ${dist}"
+  docker image tag "${repository}:${dist}" "${old_repo}:${dist}"
+  docker push "${old_repo}:${dist}"
 done
 
 log "creating docker manifest"
@@ -59,4 +70,18 @@ docker manifest create   "${repository}:latest"      \
 log "pushing docker manifest"
 docker manifest push "${repository}:latest" --purge
 
+# @afiune let us continue posting the Lacework CLI to the old reporitory for a few
+log "creating docker manifest for the old repository ${old_repo}"
+docker manifest create   "${old_repo}:latest"      \
+                         "${old_repo}:scratch"     \
+                         "${old_repo}:ubi-8"       \
+                         "${old_repo}:centos-8"    \
+                         "${old_repo}:debian-10"   \
+                         "${old_repo}:ubuntu-1804" \
+                         "${old_repo}:amazonlinux-2" --amend
+
+log "pushing docker manifest for the old repository ${old_repo}"
+docker manifest push "${old_repo}:latest" --purge
+
 log "All docker containers have been released! (https://hub.docker.com/repository/docker/${repository})"
+log "All docker containers have been released! (https://hub.docker.com/repository/docker/${old_repo})"
