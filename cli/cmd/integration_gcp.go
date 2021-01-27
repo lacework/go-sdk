9e067e6949579323bb0685d74ddf3c8bd66fa058
Darren <75614232+dmurray-lacework@users.noreply.github.com>
2021-01-27 15:48:15 +0000

    style: Rewording of private key survey message
    Co-authored-by: Scott Ford <scott.ford@lacework.net>

--- cli/cmd/integration_gcp.go
+++ cli/cmd/integration_gcp.go
@@ -48,7 +48,7 @@ func createGcpConfigIntegration() error {
 		},
 		{
 			Name:     "private_key",
-			Prompt:   &survey.Editor{Message: "Enter correctly formatted Private Key:"},
+			Prompt:   &survey.Editor{Message: "Enter properly formatted Private Key:"},
 			Validate: survey.Required,
 		},
 		{
