15c371cc59ae9b2cbbe25588ff20574c1272efbe
Salim Afiune Maya <afiune@lacework.net>
2021-01-29 14:49:20 -0600

    test: fix intermittent events test
    Signed-off-by: Salim Afiune Maya <afiune@lacework.net>

--- integration/event_test.go
+++ integration/event_test.go
@@ -19,6 +19,7 @@
 package integration
 
 import (
+	"strings"
 	"testing"
 	"time"
 
@@ -58,26 +59,30 @@ func TestEventCommandList(t *testing.T) {
 		"EXITCODE is not the expected one")
 }
 
-func TestEventCommandList3Days(t *testing.T) {
+func TestEventCommandList4Days(t *testing.T) {
 	// @afiune could we find a way to generate a consistent event? but if we do
 	// wouldn't the ML learn it and then become a known behavior? uhmmm
 	// for now we will just check that we have the headers :wink:
-	out, err, exitcode := LaceworkCLIWithTOMLConfig("event", "list", "--days", "3")
-	assert.Contains(t, out.String(), "EVENT ID",
-		"STDOUT table headers changed, please check")
-	assert.Contains(t, out.String(), "TYPE",
-		"STDOUT table headers changed, please check")
-	assert.Contains(t, out.String(), "SEVERITY",
-		"STDOUT table headers changed, please check")
-	assert.Contains(t, out.String(), "START TIME",
-		"STDOUT table headers changed, please check")
-	assert.Contains(t, out.String(), "END TIME",
-		"STDOUT table headers changed, please check")
+	out, err, exitcode := LaceworkCLIWithTOMLConfig("event", "list", "--days", "4")
 	assert.Empty(t,
 		err.String(),
 		"STDERR should be empty")
 	assert.Equal(t, 0, exitcode,
 		"EXITCODE is not the expected one")
+
+	// only verify the table headers if there are events
+	if !strings.Contains(out.String(), "There are no events in your account in the specified time range.") {
+		assert.Contains(t, out.String(), "EVENT ID",
+			"STDOUT table headers changed, please check")
+		assert.Contains(t, out.String(), "TYPE",
+			"STDOUT table headers changed, please check")
+		assert.Contains(t, out.String(), "SEVERITY",
+			"STDOUT table headers changed, please check")
+		assert.Contains(t, out.String(), "START TIME",
+			"STDOUT table headers changed, please check")
+		assert.Contains(t, out.String(), "END TIME",
+			"STDOUT table headers changed, please check")
+	}
 }
 
 func TestEventCommandListSeverityError(t *testing.T) {
