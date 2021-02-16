8e0071a7d8a879d59c4545c8528e01287b90041e
Darren <75614232+dmurray-lacework@users.noreply.github.com>
2021-02-16 20:26:19 +0000

    feat(cli): New CiscoWebex alert channel (#316)
    Signed-off-by: Darren Murray <darren.murray@lacework.net>

--- api/integrations.go
+++ api/integrations.go
@@ -61,6 +61,9 @@ const (
 	// Azure Activity Log integration type
 	AzureActivityLogIntegration
 
+	// Cisco Webex integration type
+	CiscoWebexChannelIntegration
+
 	// Container registry integration type
 	ContainerRegistryIntegration
 
@@ -95,6 +98,7 @@ var IntegrationTypes = map[integrationType]string{
 	AwsCfgIntegration:                "AWS_CFG",
 	AwsCloudTrailIntegration:         "AWS_CT_SQS",
 	AwsS3ChannelIntegration:          "AWS_S3",
+	CiscoWebexChannelIntegration:     "CISCO_SPARK_WEBHOOK",
 	DatadogChannelIntegration:        "DATADOG",
 	GcpCfgIntegration:                "GCP_CFG",
 	GcpAuditLogIntegration:           "GCP_AT_SES",
