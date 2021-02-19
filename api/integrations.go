0e9f6f57e16e96ccfb84b9bd618e595378cf0fb3
Darren <75614232+dmurray-lacework@users.noreply.github.com>
2021-02-19 14:26:51 +0000

    feat(cli): New IBM QRadar alert channel (#325)
    Signed-off-by: Darren Murray <darren.murray@lacework.net>

--- api/integrations.go
+++ api/integrations.go
@@ -73,6 +73,9 @@ const (
 	// Microsoft Teams channel integration type
 	MicrosoftTeamsChannelIntegration
 
+	// QRadar channel integration type
+	QRadarChannelIntegration
+
 	// Slack channel integration type
 	SlackChannelIntegration
 
@@ -113,6 +116,7 @@ var IntegrationTypes = map[integrationType]string{
 	AzureCfgIntegration:              "AZURE_CFG",
 	AzureActivityLogIntegration:      "AZURE_AL_SEQ",
 	ContainerRegistryIntegration:     "CONT_VULN_CFG",
+	QRadarChannelIntegration:         "IBM_QRADAR",
 	MicrosoftTeamsChannelIntegration: "MICROSOFT_TEAMS",
 	SlackChannelIntegration:          "SLACK_CHANNEL",
 	SplunkIntegration:                "SPLUNK_HEC",
--- cli/cmd/integration.go
+++ cli/cmd/integration.go
@@ -209,6 +209,7 @@ func promptCreateIntegration() error {
 				"Webhook Alert Channel",
 				"VictorOps Alert Channel",
 				"Splunk Alert Channel",
+				"QRadar Alert Channel",
 				"Service Now Alert Channel",
 				"PagerDuty Alert Channel",
 				"AWS CloudWatch Alert Channel",
@@ -256,6 +257,8 @@ func promptCreateIntegration() error {
 		return createSplunkIntegration()
 	case "PagerDuty Alert Channel":
 		return createPagerDutyAlertChannelIntegration()
+	case "QRadar Alert Channel":
+		return createQRadarAlertChannelIntegration()
 	case "Service Now Alert Channel":
 		return createServiceNowAlertChannelIntegration()
 	case "AWS CloudWatch Alert Channel":
@@ -551,6 +554,26 @@ func reflectIntegrationData(raw api.RawIntegration) [][]string {
 			[]string{"ROLE ARN", iData.Credentials.RoleArn},
 			[]string{"BUCKET ARN", iData.Credentials.BucketArn},
 			[]string{"EXTERNAL ID", iData.Credentials.ExternalID},
+		}
+
+		return out
+
+	case api.QRadarChannelIntegration.String():
+
+		var iData api.QRadarChannelData
+		err := mapstructure.Decode(raw.Data, &iData)
+		if err != nil {
+			cli.Log.Debugw("unable to decode integration data",
+				"integration_type", raw.Type,
+				"raw_data", raw.Data,
+				"error", err,
+			)
+			break
+		}
+		out := [][]string{
+			[]string{"HOST PORT", fmt.Sprint(iData.HostPort)},
+			[]string{"HOST URL", iData.HostURL},
+			[]string{"COMMUNICATION TYPE", string(iData.CommunicationType)},
 		}
 
 		return out
