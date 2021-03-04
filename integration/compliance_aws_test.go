e0fdef3891d1cf9114bc7cf5604e9a695cd5a3f8
Darren Murray <darren.murray@lacework.net>
2021-03-04 19:06:24 +0000

    test: include table headers in compliance report integration tests
    Signed-off-by: Darren Murray <darren.murray@lacework.net>

--- integration/compliance_aws_test.go
+++ integration/compliance_aws_test.go
@@ -27,8 +27,60 @@ import (
 func TestComplianceAwsGetReportFilter(t *testing.T) {
 	account := os.Getenv("LW_INT_TEST_AWS_ACC")
 	detailsOutput := "recommendations showing"
-	out, err, exitcode := LaceworkCLIWithTOMLConfig("compliance", "aws", "get-report", account, "--details", "--status", "could-not-assess")
+	out, err, exitcode := LaceworkCLIWithTOMLConfig("compliance", "aws", "get-report", account, "--status", "compliant")
+
 	assert.Contains(t, out.String(), detailsOutput, "Filtered detail output should contain filtered result")
 	assert.Empty(t, err.String(), "STDERR should be empty")
 	assert.Equal(t, 0, exitcode, "EXITCODE is not the expected one")
+	assert.Contains(t, out.String(), "COMPLIANCE REPORT DETAILS",
+		"STDOUT table headers changed, please check")
+	assert.Contains(t, out.String(), account,
+		"Account ID in compliance report is not correct")
+	assert.Contains(t, out.String(), "NON-COMPLIANT RECOMMENDATIONS",
+		"STDOUT table headers changed, please check")
+	assert.Contains(t, out.String(), "ID",
+		"STDOUT table headers changed, please check")
+	assert.Contains(t, out.String(), "RECOMMENDATION",
+		"STDOUT table headers changed, please check")
+	assert.Contains(t, out.String(), "STATUS",
+		"STDOUT table headers changed, please check")
+	assert.Contains(t, out.String(), "SEVERITY",
+		"STDOUT table headers changed, please check")
+	assert.Contains(t, out.String(), "SERVICE",
+		"STDOUT table headers changed, please check")
+	assert.Contains(t, out.String(), "AFFECTED",
+		"STDOUT table headers changed, please check")
+	assert.Contains(t, out.String(), "ASSESSED",
+		"STDOUT table headers changed, please check")
+}
+
+func TestComplianceAwsGetReportDetails(t *testing.T) {
+	account := os.Getenv("LW_INT_TEST_AWS_ACC")
+	detailsOutput := "recommendations showing"
+	out, err, exitcode := LaceworkCLIWithTOMLConfig("compliance", "aws", "get-report", account, "--details")
+
+	assert.NotContains(t, out.String(), detailsOutput,
+		"Details table without filter should not contain filtered output")
+	assert.Empty(t, err.String(), "STDERR should be empty")
+	assert.Equal(t, 0, exitcode, "EXITCODE is not the expected one")
+	assert.Contains(t, out.String(), "COMPLIANCE REPORT DETAILS",
+		"STDOUT table headers changed, please check")
+	assert.Contains(t, out.String(), account,
+		"Account ID in compliance report is not correct")
+	assert.Contains(t, out.String(), "NON-COMPLIANT RECOMMENDATIONS",
+		"STDOUT table headers changed, please check")
+	assert.Contains(t, out.String(), "ID",
+		"STDOUT table headers changed, please check")
+	assert.Contains(t, out.String(), "RECOMMENDATION",
+		"STDOUT table headers changed, please check")
+	assert.Contains(t, out.String(), "STATUS",
+		"STDOUT table headers changed, please check")
+	assert.Contains(t, out.String(), "SEVERITY",
+		"STDOUT table headers changed, please check")
+	assert.Contains(t, out.String(), "SERVICE",
+		"STDOUT table headers changed, please check")
+	assert.Contains(t, out.String(), "AFFECTED",
+		"STDOUT table headers changed, please check")
+	assert.Contains(t, out.String(), "ASSESSED",
+		"STDOUT table headers changed, please check")
 }
