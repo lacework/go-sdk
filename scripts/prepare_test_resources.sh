10c5aedf0a557e6a50c78bf49af7e079621ba31f
Darren Murray <darren.murray@lacework.net>
2021-03-18 15:54:49 +0000

    test: Change target for container vuln scanning tests
    Signed-off-by: Darren Murray <darren.murray@lacework.net>

---
+++ scripts/prepare_test_resources.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# Name::        prepare_test_resources.sh
+# Description:: Make ready resources required by integration tests
+# Author::      Darren Murray (<darren.murray@lacework.net>)
+#
+
+
