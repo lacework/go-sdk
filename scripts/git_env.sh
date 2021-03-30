ab9a4787d8fb85eebc844c62c8ddd3eb1b7f2769
Darren Murray <darren.murray@lacework.net>
2021-03-30 10:18:13 +0100

    fix: Set path to hooks folder instead of update script
    Signed-off-by: Darren Murray <darren.murray@lacework.net>

--- scripts/git_env.sh
+++ scripts/git_env.sh
@@ -1,40 +1,22 @@
 #!/bin/bash
 #
 # Name::        git_env.sh
-# Description:: Use this script to configure local git env,
-#               including commit message validation
+# Description:: Use this script to configure local git env
 # Author::      Darren Murray (<dmurray-lacework@lacework.net>)
 #
 
-readonly commit_hook=.git/hooks/prepare-commit-msg
-readonly new_commit_hook=scripts/githooks/prepare-commit-msg
+readonly commit_hook=scripts/githooks/prepare-commit-msg
+readonly hooks_path=scripts/githooks/
 readonly project_name=go-sdk
 
-create_prepare_commit_msg(){
-  if [ ! -f $commit_hook ]; then
-    log "Adding git commit hooks"
-    cp scripts/githooks/prepare-commit-msg $commit_hook
-    chmod +x $commit_hook
-  else 
-    log "Git commit hook already exists"
-    update
-  fi
-}
-
-update() {
-  currentVersion=$(cat $commit_hook | grep 'readonly version=v'| sed 's/.*readonly version=\(.*\)/\1/')
-  newVersion=$(cat $new_commit_hook | grep 'readonly version=v'| sed 's/.*readonly version=\(.*\)/\1/')
-    if [ $newVersion != $currentVersion ]; then
-        log "Updating git commit hooks version $currentVersion -> $newVersion"
-        cp scripts/githooks/prepare-commit-msg $commit_hook
-        chmod +x $commit_hook
-    else 
-        log "No changes made to version"
-  fi
+prepare_git_env(){
+  chmod +x $commit_hook
+  log "Setting git-hooks path to $hooks_path"
+  git config core.hooksPath $hooks_path
 }
 
 log() {
   echo "--> ${project_name}: $1"
 }
 
-create_prepare_commit_msg
+prepare_git_env
