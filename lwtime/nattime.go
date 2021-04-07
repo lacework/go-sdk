ca17f245a5d2a150f2c64d15cd47d753a01aa66b
David Hazekamp <david.hazekamp@lacework.net>
2021-04-07 09:34:14 -0500

    feat(cli): Natural/Relative time package
    1.  Introduce natural and relative time package
    
    ALLY-427

---
+++ lwtime/nattime.go
@@ -0,0 +1,191 @@
+//
+// Author:: Salim Afiune Maya (<afiune@lacework.net>)
+// Copyright:: Copyright 2020, Lacework Inc.
+// License:: Apache License, Version 2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package lwtime
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/pkg/errors"
+)
+
+type NatTimeAdjective string
+
+const (
+	// both plural and singular regular expressions should contain 3 capture groups
+	natTimePluralRE   string = `^(last)\s(\d+)\s(years|months|weeks|days|hours|minutes|seconds)$`
+	natTimeSingularRE string = `^(this|current|previous|last)(\s)(year|month|week|day|hour|minute|second)$`
+
+	Today     NatTimeAdjective = "today"
+	Yesterday NatTimeAdjective = "yesterday"
+	This      NatTimeAdjective = "this"
+	Current   NatTimeAdjective = "current"
+	Previous  NatTimeAdjective = "previous"
+	Last      NatTimeAdjective = "last"
+)
+
+func (nta NatTimeAdjective) IsValid() bool {
+	switch NatTimeAdjective(strings.ToLower(string(nta))) { // inline lowercase conversion
+	case Today, Yesterday, This, Current, Previous, Last:
+		return true
+	}
+	return false
+}
+
+type NatTime struct {
+	Adjective NatTimeAdjective
+	Num       string
+	INum      int
+	Unit      RelTimeUnit
+}
+
+func (nt *NatTime) LoadRelTimeUnit(u string) bool {
+	switch strings.ToLower(u) {
+	case "year", "years":
+		nt.Unit = Year
+	case "month", "months":
+		nt.Unit = Month
+	case "week", "weeks":
+		nt.Unit = Week
+	case "day", "days":
+		nt.Unit = Day
+	case "hour", "hours":
+		nt.Unit = Hour
+	case "minute", "minutes":
+		nt.Unit = Minute
+	case "second", "seconds":
+		nt.Unit = Second
+	default:
+		return false
+	}
+	return true
+}
+
+func (nt *NatTime) Parse(s string) (err error) {
+	s = strings.ToLower(s)
+	// Today
+	if NatTimeAdjective(s) == Today {
+		nt.Adjective = This
+		nt.Num = "1"
+		nt.INum = 1
+		nt.Unit = RelTimeUnit("d")
+		return nil
+	}
+	// Yesterday
+	if NatTimeAdjective(s) == Yesterday {
+		nt.Adjective = Previous
+		nt.Num = "1"
+		nt.INum = 1
+		nt.Unit = RelTimeUnit("d")
+		return nil
+	}
+	// Singular
+	var nt_parts []string
+	singularRE := regexp.MustCompile(natTimeSingularRE)
+	if nt_parts = singularRE.FindStringSubmatch(s); s == "" || nt_parts == nil {
+		// Plural
+		pluralRE := regexp.MustCompile(natTimePluralRE)
+		if nt_parts = pluralRE.FindStringSubmatch(s); s == "" || nt_parts == nil {
+			return errors.New(fmt.Sprintf("natural time (%s) is invalid", s))
+		}
+	}
+	// Adjective
+	nt.Adjective = NatTimeAdjective(nt_parts[1])
+	if !nt.Adjective.IsValid() {
+		// this would indicate a code mismatch between enumerated Adjectives and Regex
+		return errors.New(fmt.Sprintf("invalid adjective for natural time (%s)", s))
+	}
+	// Num
+	nt.Num = nt_parts[2]
+	nt.INum, err = strconv.Atoi(nt.Num)
+	if err != nil {
+		nt.Num = "1"
+		nt.INum = 1
+	}
+	// Unit
+	if ok := nt.LoadRelTimeUnit(nt_parts[3]); !ok {
+		return errors.New(fmt.Sprintf("invalid unit for natural time (%s)", s))
+	}
+	return nil
+}
+
+type NatTimeRange struct {
+	Start string
+	End   string
+}
+
+func (ntr *NatTimeRange) Parse(s string) (err error) {
+	nt := NatTime{}
+	if err = nt.Parse(s); err != nil {
+		return err
+	}
+
+	switch nt.Adjective {
+	case This, Current:
+		ntr.Start = fmt.Sprintf("@%s", nt.Unit)
+		ntr.End = "now"
+	case Previous:
+		ntr.Start = fmt.Sprintf("-1%s@%s", nt.Unit, nt.Unit)
+		ntr.End = fmt.Sprintf("@%s", nt.Unit)
+	case Last:
+		ntr.Start = fmt.Sprintf("-%s%s", nt.Num, nt.Unit)
+		ntr.End = "now"
+	default:
+		// This would indicate a code mismatch between NatTime.Parse() and NatTimeRange.Parse()
+		return errors.New(
+			fmt.Sprintf("invalid adjective for natural time (%s)", s))
+	}
+	return nil
+}
+
+func (ntr NatTimeRange) Range() (start time.Time, end time.Time, err error) {
+	return ntr.Range_(time.Now())
+}
+
+func (ntr NatTimeRange) Range_(t time.Time) (start time.Time, end time.Time, err error) {
+	baseErr := "unable to compute natural time range"
+	rt := RelTime{}
+
+	// start
+	if err = rt.Parse(ntr.Start); err != nil {
+		return start, end, errors.Wrap(
+			errors.New(fmt.Sprintf(
+				"invalid relative time (%s) for range start", ntr.Start)),
+			baseErr,
+		)
+	}
+	if start, err = rt.Time_(t); err != nil {
+		return start, end, errors.Wrap(err, baseErr)
+	}
+	// end
+	if err = rt.Parse(ntr.End); err != nil {
+		return start, end, errors.Wrap(
+			errors.New(fmt.Sprintf(
+				"invalid relative time (%s) for range end", ntr.End)),
+			baseErr,
+		)
+	}
+	if end, err = rt.Time_(t); err != nil {
+		return start, end, err
+	}
+	return start, end, err
+}
---
+++ lwtime/reltime.go
@@ -0,0 +1,204 @@
+//
+// Author:: Salim Afiune Maya (<afiune@lacework.net>)
+// Copyright:: Copyright 2020, Lacework Inc.
+// License:: Apache License, Version 2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package lwtime
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/pkg/errors"
+)
+
+type RelDate struct {
+	Year  int
+	Month time.Month
+	Day   int
+}
+
+func Mondays(year int) (mondays []RelDate) {
+	start := time.Date(year, 1, 1, 0, 0, 0, 0, time.Now().Location())
+	d, _ := time.ParseDuration("24h")
+	startYear := start.Year()
+	for startYear == start.Year() {
+		if start.Weekday() == time.Monday {
+			year, month, day := start.Date()
+			mondays = append(mondays, RelDate{year, month, day})
+		}
+		start = start.Add(d)
+	}
+	return mondays
+}
+
+type RelTimeUnit string
+
+const (
+	relTimeRE string      = `^([+-])?(?:(\d+)(\w+))?(?:@(\w+))?$`
+	Year      RelTimeUnit = "y"
+	Month     RelTimeUnit = "mon"
+	Week      RelTimeUnit = "w"
+	Day       RelTimeUnit = "d"
+	Hour      RelTimeUnit = "h"
+	Minute    RelTimeUnit = "m"
+	Second    RelTimeUnit = "s"
+)
+
+func (rtu RelTimeUnit) IsValid() bool {
+	switch RelTimeUnit(strings.ToLower(string(rtu))) { // inline lowercase conversion
+	case Year, Month, Week, Day, Hour, Minute, Second, RelTimeUnit(""):
+		return true
+	}
+	return false
+}
+
+func (rtu RelTimeUnit) SnapTime(inTime time.Time) (outTime time.Time, err error) {
+	// immediately short circuit if snap is invalid
+	if !rtu.IsValid() {
+		return outTime, errors.New(
+			fmt.Sprintf("snap (%s) is not a valid relative time unit", rtu))
+	}
+
+	year, month, day := inTime.Date()
+	hour := inTime.Hour()
+	minute := inTime.Minute()
+	second := inTime.Second()
+	nano := inTime.Nanosecond()
+
+	switch RelTimeUnit(strings.ToLower(string(rtu))) {
+	case Week:
+		year, week := inTime.ISOWeek()
+		relDate := Mondays(year)[week-1]
+		outTime = time.Date(
+			relDate.Year,
+			relDate.Month,
+			relDate.Day,
+			0, 0, 0, 0, inTime.Location(),
+		)
+		return outTime, nil
+	case Year:
+		month = 1
+		fallthrough
+	case Month:
+		day = 1
+		fallthrough
+	case Day:
+		hour = 0
+		fallthrough
+	case Hour:
+		minute = 0
+		fallthrough
+	case Minute:
+		second = 0
+		fallthrough
+	case Second:
+		nano = 0
+	}
+	return time.Date(year, month, day, hour, minute, second, nano, inTime.Location()), nil
+}
+
+type RelTime struct {
+	Num  string
+	INum int
+	Unit RelTimeUnit
+	Snap RelTimeUnit
+}
+
+func (rt *RelTime) Parse(s string) (err error) {
+	var rt_parts []string
+	// now is equivelant to +0s
+	if s == "now" {
+		s = "+0s"
+	}
+	// regex
+	re := regexp.MustCompile(relTimeRE)
+	if rt_parts = re.FindStringSubmatch(s); s == "" || rt_parts == nil {
+		return errors.New(fmt.Sprintf("relative time specifier (%s) is invalid", s))
+	}
+	// Num
+	if rt_parts[1] == "-" {
+		rt.Num = rt_parts[1] + rt_parts[2]
+	} else {
+		rt.Num = rt_parts[2]
+	}
+	rt.INum, err = strconv.Atoi(rt.Num)
+	if err != nil {
+		rt.Num = "0"
+		rt.INum = 0
+	}
+	// Unit
+	rt.Unit = RelTimeUnit(strings.ToLower(string(rt_parts[3])))
+	if !rt.Unit.IsValid() {
+		return errors.New(fmt.Sprintf("invalid unit for relative time specifier (%s)", s))
+	}
+	if rt.Unit == RelTimeUnit("") {
+		rt.Unit = Second
+	}
+	// Weeeeeeek
+	if rt.Unit == Week {
+		rt.INum = rt.INum * 7
+		rt.Num = strconv.Itoa(rt.INum)
+		rt.Unit = Day
+	}
+	// Snap
+	rt.Snap = RelTimeUnit(strings.ToLower(string(rt_parts[4])))
+	if !rt.Snap.IsValid() {
+		return errors.New(fmt.Sprintf("invalid snap for relative time specifier (%s)", s))
+	}
+	return nil
+}
+
+func (rt RelTime) Time() (t time.Time, err error) {
+	return rt.Time_(time.Now())
+}
+
+func (rt RelTime) Time_(inTime time.Time) (outTime time.Time, err error) {
+	baseErr := "unable to construct time object"
+	switch rt.Unit {
+	case Year:
+		outTime = inTime.AddDate(rt.INum, 0, 0)
+	case Month:
+		outTime = inTime.AddDate(0, rt.INum, 0)
+	case Day:
+		outTime = inTime.AddDate(0, 0, rt.INum)
+	case Hour, Minute, Second:
+		var d time.Duration
+		d, err = time.ParseDuration(fmt.Sprintf("%s%s", rt.Num, rt.Unit))
+		if err != nil {
+			return outTime, err
+		}
+		outTime = inTime.Add(d)
+	default:
+		return outTime, errors.Wrap(
+			errors.New(fmt.Sprintf("relative time unit (%s) is invalid", rt.Unit)),
+			baseErr,
+		)
+	}
+	if rt.Snap != "" {
+		outTime, err = rt.Snap.SnapTime(outTime)
+	}
+	if err != nil {
+		return outTime, errors.Wrap(err, baseErr)
+	}
+	if outTime.Unix() < 0 {
+		return outTime, errors.Wrap(errors.New("time predates epoch"), baseErr)
+	}
+	return outTime, err
+}
