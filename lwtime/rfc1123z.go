1175ec8b2346887c0a8e312762b9e52ebd512ffd
Darren Murray <darren.murray@lacework.net>
2021-06-17 12:11:50 +0100

    fix: first_time_seen json output returns RFC3339
    Signed-off-by: Darren Murray <darren.murray@lacework.net>

--- lwtime/rfc1123z.go
+++ lwtime/rfc1123z.go
@@ -5,16 +5,25 @@ import (
 	"time"
 )
 
-type RFC1123Z struct {
-	time.Time
-}
+type RFC1123Z time.Time
 
 func (rfc *RFC1123Z) UnmarshalJSON(b []byte) (err error) {
 	t := strings.Trim(string(b), `"`)
-	rfc.Time, err = time.Parse(time.RFC1123Z, t)
+	res, _ := time.Parse(time.RFC1123Z, t)
+	*rfc = RFC1123Z(res)
 	return
 }
 
 func (rfc *RFC1123Z) MarshalJSON() ([]byte, error) {
-	return []byte(`"` + rfc.Time.Format(time.RFC1123Z) + `"`), nil
+	return rfc.ToTime().UTC().MarshalJSON()
+}
+
+func (rfc RFC1123Z) ToTime() time.Time {
+	return time.Time(rfc)
+}
+func (rfc RFC1123Z) Format(s string) string {
+	return rfc.ToTime().Format(s)
+}
+func (rfc RFC1123Z) UTC() time.Time {
+	return rfc.ToTime().UTC()
 }
--- lwtime/rfc1123z_test.go
+++ lwtime/rfc1123z_test.go
@@ -18,5 +18,14 @@ func TestUnmarshallRFC1123Z(t *testing.T) {
 	jsonString := fmt.Sprintf(`{"first_seen_time": "%s"}`, timeString)
 	res := mockFirstSeenResponse{}
 	json.Unmarshal([]byte(jsonString), &res)
-	assert.Equal(t, timeString, res.FirstTimeSeen.Time.Format(time.RFC1123Z), "failed to parse RFC1123Z time")
+	assert.Equal(t, timeString, res.FirstTimeSeen.Format(time.RFC1123Z), "failed to parse RFC1123Z time")
+}
+
+func TestMarshallRFC1123Z(t *testing.T) {
+	timeString, _ := time.Parse(time.RFC1123Z, "Mon, 12 Apr 2021 20:00:00 -0700")
+	expectedJson := "{\"first_seen_time\":\"2021-04-13T03:00:00Z\"}"
+	res := mockFirstSeenResponse{FirstTimeSeen: RFC1123Z(timeString)}
+
+	marshalled, _ := json.Marshal(&res)
+	assert.Equal(t, expectedJson, string(marshalled), "first_seen_time output is not RFC3339")
 }
