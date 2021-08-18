d2cec5dd0429bfe85039012da5c8fceb7b3af9ab
Darren Murray <darren.murray@lacework.net>
2021-08-18 13:13:35 +0100

    fix: Detect if running in cloudshell to output correct update command
    Signed-off-by: Darren Murray <darren.murray@lacework.net>

--- cli/cmd/cli_unix.go
+++ cli/cmd/cli_unix.go
@@ -33,6 +33,13 @@ var promptIconsFunc = func(icons *survey.IconSet) {
 	icons.Question.Text = "▸"
 }
 
+// Env variables found in GCP, AWS and Azure cloudshell
+var (
+	gcpCloudEnv   = "CLOUD_SHELL"
+	awsCloudEnv   = "AWS_EXECUTION_ENV"
+	AzureCloudEnv = "POWERSHELL_DISTRIBUTION_CHANNEL"
+)
+
 // UpdateCommand returns the command that a user should run to update the cli
 // to the latest available version (unix specific command)
 func (c *cliState) UpdateCommand() string {
@@ -41,7 +48,17 @@ func (c *cliState) UpdateCommand() string {
   brew upgrade lacework-cli
 `
 	}
+
+	if isCloudshell() {
+		return `
+  curl https://raw.githubusercontent.com/lacework/go-sdk/main/cli/install.sh | bash -s -- -d $HOME/bin
+`
+	}
 	return `
   curl https://raw.githubusercontent.com/lacework/go-sdk/main/cli/install.sh | bash
 `
+}
+
+func isCloudshell() bool {
+	return os.Getenv(gcpCloudEnv) == "true" || os.Getenv(awsCloudEnv) == "Cloudshell" || os.Getenv(AzureCloudEnv) == "CloudShell"
 }
