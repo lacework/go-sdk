8823b3e464800aa0b270474e330ee0191d33d701
Darren Murray <darren.murray@lacework.net>
2021-10-01 15:16:04 +0100

    feat: Webhook alert channel(v2)
    Signed-off-by: Darren Murray <darren.murray@lacework.net>

---
+++ api/alert_channels_webhook.go
@@ -0,0 +1,51 @@
+//
+// Author:: Darren Murray (<darren.murray@lacework.net>)
+// Copyright:: Copyright 2021, Lacework Inc.
+// License:: Apache License, Version 2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package api
+
+// GetWebhook gets a single Webhook alert channel matching the
+// provided integration guid
+func (svc *AlertChannelsService) GetWebhook(guid string) (
+	response WebhookAlertChannelResponseV2,
+	err error,
+) {
+	err = svc.get(guid, &response)
+	return
+}
+
+// UpdateWebhook updates a single Webhook integration on the Lacework Server
+func (svc *AlertChannelsService) UpdateWebhook(data AlertChannel) (
+	response WebhookAlertChannelResponseV2,
+	err error,
+) {
+	err = svc.update(data.ID(), data, &response)
+	return
+}
+
+type WebhookAlertChannelResponseV2 struct {
+	Data WebhookAlertChannelV2 `json:"data"`
+}
+
+type WebhookAlertChannelV2 struct {
+	v2CommonIntegrationData
+	Data WebhookDataV2 `json:"data"`
+}
+
+type WebhookDataV2 struct {
+	Url string `json:"webhookUrl"`
+}
---
+++ api/alert_channels_webhook_test.go
@@ -0,0 +1,135 @@
+//
+// Author:: Darren Murray (<darren.murray@lacework.net>)
+// Copyright:: Copyright 2021, Lacework Inc.
+// License:: Apache License, Version 2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package api_test
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/lacework/go-sdk/api"
+	"github.com/lacework/go-sdk/internal/intgguid"
+	"github.com/lacework/go-sdk/internal/lacework"
+)
+
+func TestAlertChannelsGetWebhook(t *testing.T) {
+	var (
+		intgGUID   = intgguid.New()
+		apiPath    = fmt.Sprintf("AlertChannels/%s", intgGUID)
+		fakeServer = lacework.MockServer()
+	)
+	fakeServer.UseApiV2()
+	fakeServer.MockToken("TOKEN")
+	defer fakeServer.Close()
+
+	fakeServer.MockAPI(apiPath, func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "GET", r.Method, "GetWebhook() should be a GET method")
+		fmt.Fprintf(w, generateAlertChannelResponse(singleWebhookAlertChannel(intgGUID)))
+	})
+
+	c, err := api.NewClient("test",
+		api.WithApiV2(),
+		api.WithToken("TOKEN"),
+		api.WithURL(fakeServer.URL()),
+	)
+	assert.Nil(t, err)
+
+	response, err := c.V2.AlertChannels.GetWebhook(intgGUID)
+	assert.Nil(t, err)
+	assert.NotNil(t, response)
+	assert.Equal(t, intgGUID, response.Data.IntgGuid)
+	assert.Equal(t, "integration_name", response.Data.Name)
+	assert.True(t, response.Data.State.Ok)
+	assert.Contains(t, response.Data.Data.Url, "https://hooks.webhook.com/?api-token=12345")
+}
+
+func TestAlertChannelWebhookUpdate(t *testing.T) {
+	var (
+		intgGUID   = intgguid.New()
+		apiPath    = fmt.Sprintf("AlertChannels/%s", intgGUID)
+		fakeServer = lacework.MockServer()
+	)
+	fakeServer.UseApiV2()
+	fakeServer.MockToken("TOKEN")
+	defer fakeServer.Close()
+
+	fakeServer.MockAPI(apiPath, func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "PATCH", r.Method, "UpdateWebhook() should be a PATCH method")
+
+		if assert.NotNil(t, r.Body) {
+			body := httpBodySniffer(r)
+			assert.Contains(t, body, intgGUID, "INTG_GUID missing")
+			assert.Contains(t, body, "integration_name", "alert channel name is missing")
+			assert.Contains(t, body, "Webhook", "wrong alert channel type")
+			assert.Contains(t, body, "https://hooks.webhook.com/?api-token=12345", "missing webhook url")
+		}
+
+		fmt.Fprintf(w, generateAlertChannelResponse(singleWebhookAlertChannel(intgGUID)))
+	})
+
+	c, err := api.NewClient("test",
+		api.WithApiV2(),
+		api.WithToken("TOKEN"),
+		api.WithURL(fakeServer.URL()),
+	)
+	assert.Nil(t, err)
+
+	webhookAlertChan := api.NewAlertChannel("integration_name",
+		api.WebhookAlertChannelType,
+		api.WebhookDataV2{
+			Url: "https://hooks.webhook.com/?api-token=12345",
+		},
+	)
+	assert.Equal(t, "integration_name", webhookAlertChan.Name, "Webhook alert channel name mismatch")
+	assert.Equal(t, "Webhook", webhookAlertChan.Type, "a new Webhook alert channel should match its type")
+	assert.Equal(t, 1, webhookAlertChan.Enabled, "a new Webhook alert channel should be enabled")
+	webhookAlertChan.IntgGuid = intgGUID
+
+	response, err := c.V2.AlertChannels.UpdateWebhook(webhookAlertChan)
+	assert.Nil(t, err)
+	assert.NotNil(t, response)
+	assert.Equal(t, intgGUID, response.Data.IntgGuid)
+	assert.True(t, response.Data.State.Ok)
+	assert.Contains(t, response.Data.Data.Url, "https://hooks.webhook.com/?api-token=12345")
+}
+
+func singleWebhookAlertChannel(id string) string {
+	return `
+{
+    "createdOrUpdatedBy": "salim.afiunemaya@lacework.net",
+    "createdOrUpdatedTime": "2021-06-01T18:10:40.745Z",
+    "data": {
+	  "webhookUrl": "https://hooks.webhook.com/?api-token=12345"
+    },
+    "enabled": 1,
+    "intgGuid": "` + id + `",
+    "isOrg": 0,
+    "name": "integration_name",
+    "state": {
+      "details": {},
+      "lastSuccessfulTime": 1627895573122,
+      "lastUpdatedTime": 1627895573122,
+      "ok": true
+    },
+    "type": "Webhook"
+}
+  `
+}
