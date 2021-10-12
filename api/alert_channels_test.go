d7bd6270960400dc5df0b90d7c1ee723f262ceb7
Darren Murray <darren.murray@lacework.net>
2021-10-12 17:49:23 +0100

    style: formatting
    Signed-off-by: Darren Murray <darren.murray@lacework.net>

--- api/alert_channels_test.go
+++ api/alert_channels_test.go
@@ -256,7 +256,7 @@ func TestAlertChannelsList(t *testing.T) {
 		victorOpsAlertChan         = generateGuids(&allGUIDs, 2)
 		ciscoSparkWebhookAlertChan = generateGuids(&allGUIDs, 2)
 		microsoftTeamsAlertChan    = generateGuids(&allGUIDs, 2)
-		splunkHecOpsAlertChan         = generateGuids(&allGUIDs, 2)
+		splunkHecOpsAlertChan      = generateGuids(&allGUIDs, 2)
 		expectedLen                = len(allGUIDs)
 		fakeServer                 = lacework.MockServer()
 	)
