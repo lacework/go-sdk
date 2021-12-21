b473134e45811101bf11bbb403a9521ec93400d0
Salim Afiune Maya <afiune@lacework.net>
2021-12-21 01:08:58 -0600

    ci(win): run single command
    Signed-off-by: Salim Afiune Maya <afiune@lacework.net>

--- scripts/codefresh.ps1
+++ scripts/codefresh.ps1
@@ -10,3 +10,5 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/lacework/go-sdk/main/cli/install.ps1'))
 
 lacework version
+
+lacework int list -a $env:CI_V2_ACCOUNT -k $env:CI_API_KEY -s $env:CI_API_SECRET
