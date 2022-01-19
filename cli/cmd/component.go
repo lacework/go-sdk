d4ed6681c87a461e325e4129d1540f266cd5bee2
David Hazekamp <david.hazekamp@lacework.net>
2022-01-19 12:33:41 -0600

    feat: lwcomponent library
    Fix integration test error for TestNoCommandProvided
    
    ALLY-810

--- cli/cmd/component.go
+++ cli/cmd/component.go
@@ -38,7 +38,7 @@ var (
 	componentsCmd = &cobra.Command{
 		Use:     "component",
 		Aliases: []string{"components"},
-		Short:   "manage components",
+		Short:   "Manage components",
 		Long:    `Manage components to extend your experience with the Lacework platform`,
 	}
 
