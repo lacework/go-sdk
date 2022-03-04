fff8e57218a46bc91afe70b10ce528e58d6d7c42
Salim Afiune Maya <afiune@lacework.net>
2022-03-04 12:24:55 -0800

    test(integration): update query id
    Signed-off-by: Salim Afiune Maya <afiune@lacework.net>

--- integration/lql_update_test.go
+++ integration/lql_update_test.go
@@ -27,15 +27,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestQueryUpdateHelp(t *testing.T) {
-	out, err, exitcode := LaceworkCLI("help", "query", "update")
-	assert.Contains(t, out.String(), "lacework query update [flags]")
-	assert.Contains(t, out.String(), "-f, --file string")
-	assert.Contains(t, out.String(), "-u, --url string")
-	assert.Empty(t, err.String(), "STDERR should be empty")
-	assert.Equal(t, 0, exitcode, "EXITCODE is not the expected one")
-}
-
 func TestQueryUpdateEditor(t *testing.T) {
 	out, err, exitcode := LaceworkCLIWithTOMLConfig("query", "update")
 	assert.Contains(t, out.String(), "Type a query to update")
@@ -98,4 +89,39 @@ func TestQueryUpdateURL(t *testing.T) {
 	assert.Contains(t, out.String(), "INSERT_TIME")
 	assert.Empty(t, stderr.String(), "STDERR should be empty")
 	assert.Equal(t, 0, exitcode, "EXITCODE is not the expected one")
+}
+
+func TestQueryUpdateFromIDNotFound(t *testing.T) {
+	out, stderr, exitcode := LaceworkCLIWithTOMLConfig("query", "update", "ID_NOT_FOUND", "--noninteractive")
+	assert.Empty(t, out.String(), "STDOUT should be empty") // added --noninteractive to avoid polluting STDOUT
+	assert.Contains(t, stderr.String(), "unable to load query from your account")
+	assert.Contains(t, stderr.String(), "/api/v2/Queries/ID_NOT_FOUND")
+	assert.Contains(t, stderr.String(), "Query id ID_NOT_FOUND not found")
+	assert.Equal(t, 1, exitcode, "EXITCODE is not the expected one")
+}
+
+func TestQueryUpdateFromIDEditor(t *testing.T) {
+	// get temp file
+	file, err := createTemporaryFile(
+		"TestQueryUpdateFromIDEditor",
+		fmt.Sprintf(queryJSONTemplate, evaluatorID, queryID, queryUpdateText),
+	)
+	if err != nil {
+		t.FailNow()
+	}
+	defer os.Remove(file.Name())
+
+	// setup
+	LaceworkCLIWithTOMLConfig("query", "create", "-u", queryURL)
+	// teardown
+	defer LaceworkCLIWithTOMLConfig("query", "delete", queryID)
+
+	// TODO @afiune improve interactive mode tests by mocking the terminal like we do at
+	// =>integration/aws_generate_test.go
+	out, stderr, exitcode := LaceworkCLIWithTOMLConfig("query", "update", queryID)
+	assert.Contains(t, out.String(), "Retrieving query...")
+	assert.Contains(t, out.String(), fmt.Sprintf("Update query %s", queryID))
+	assert.Contains(t, out.String(), "[Enter to launch editor]")
+	assert.Contains(t, stderr.String(), "ERROR unable to update query:")
+	assert.Equal(t, 1, exitcode, "EXITCODE is not the expected one")
 }
