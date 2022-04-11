cf40a1ad20bd455a1887b55fded03039e8c39f1c
Salim Afiune Maya <afiune@lacework.net>
2022-04-11 22:23:40 +0200

    fix(cli): format human output when needed
    Signed-off-by: Salim Afiune Maya <afiune@lacework.net>

--- cli/cmd/outputs.go
+++ cli/cmd/outputs.go
@@ -39,11 +39,15 @@ func (c *cliState) OutputJSON(v interface{}) error {
 	return nil
 }
 
-// OutputHumanRead will print out the provided message if the cli state is
+// OutputHuman will print out the provided message if the cli state is
 // configured to talk to humans, to switch to json format use --json
 func (c *cliState) OutputHuman(format string, a ...interface{}) {
 	if c.HumanOutput() {
-		fmt.Fprintf(os.Stdout, format, a...)
+		if len(a) == 0 {
+			fmt.Fprint(os.Stdout, format)
+		} else {
+			fmt.Fprintf(os.Stdout, format, a...)
+		}
 	}
 }
 
