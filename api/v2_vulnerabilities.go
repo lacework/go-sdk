b8916bf60e7930f10d30b4e313b2dbfdd5ec56b4
aircraft-cerier <58537442+aircraft-cerier@users.noreply.github.com>
2022-04-25 16:40:49 -0700

    typo in type
    Signed-off-by: aircraft-cerier <58537442+aircraft-cerier@users.noreply.github.com>

--- api/v2_vulnerabilities.go
+++ api/v2_vulnerabilities.go
@@ -83,7 +83,7 @@ func (svc *v2ContainerVulnerabilityService) SearchAllPages(filters SearchFilter)
 	for {
 		all = append(all, response.Data...)
 
-		newResponse := VulnerabilitiesContainersResponse{
+		newResponse := VulnerabilitiesHostResponse{
 			Paging: response.Paging,
 		}
 		pageOk, err = svc.client.NextPage(&newResponse)
