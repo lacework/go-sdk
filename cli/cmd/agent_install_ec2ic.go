dec996131b7e4eda7f87ebe0a3e8ad44ab4a899b
nschmeller <nick.schmeller@lacework.net>
2022-10-19 04:29:52 +0000

    Shorten lines
    Signed-off-by: nschmeller <nick.schmeller@lacework.net>

--- cli/cmd/agent_install_ec2ic.go
+++ cli/cmd/agent_install_ec2ic.go
@@ -81,7 +81,13 @@ func init() {
 	agentInstallAWSEC2ICCmd.Flags().StringVar(&agentCmdState.InstallSshUser,
 		"ssh_username", "", "username to login with",
 	)
-	agentInstallAWSEC2ICCmd.Flags().IntVarP(&agentCmdState.InstallMaxParallelism, "max_parallelism", "n", 50, "maximum number of workers executing AWS API calls, set if rate limits are lower or higher than normal")
+	agentInstallAWSSSHCmd.Flags().IntVarP(
+		&agentCmdState.InstallMaxParallelism,
+		"max_parallelism",
+		"n",
+		50,
+		"maximum number of workers executing AWS API calls, set if rate limits are lower or higher than normal",
+	)
 }
 
 func installAWSEC2IC(_ *cobra.Command, _ []string) error {
