d317e8b65a4d5217cce4cf07ed495c1feafc2224
Ammar Ekbote <ammar.ekbote@lacework.net>
2022-11-20 21:55:19 -0800

    feat(api) Adds GCP Sidekick Integration
    This features adds the Sidekick Integration type needed by terraform provider module to access the new integration.
    
    Closes Internal Ticker - #RAIN-43475

---
+++ api/cloud_accounts_gcp_sidekick.go
@@ -0,0 +1,69 @@
+//
+// Author:: Ammar Ekbote(<ammar.ekbote@lacework.net>)
+// Copyright:: Copyright 2022, Lacework Inc.
+// License:: Apache License, Version 2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package api
+
+// GetGcpSidekick gets a single GcpSidekick integration matching the provided integration guid
+func (svc *CloudAccountsService) GetGcpSidekick(guid string) (
+	response GcpSidekickIntegrationResponse,
+	err error,
+) {
+	err = svc.get(guid, &response)
+	return
+}
+
+// UpdateGcpSidekick updates a single GcpSidekick integration on the Lacework Server
+func (svc *CloudAccountsService) UpdateGcpSidekick(data CloudAccount) (
+	response GcpSidekickIntegrationResponse,
+	err error,
+) {
+	err = svc.update(data.ID(), data, &response)
+	return
+}
+
+type GcpSidekickIntegrationResponse struct {
+	Data V2GcpSidekickIntegration `json:"data"`
+}
+
+type V2GcpSidekickIntegration struct {
+	v2CommonIntegrationData
+	Data GcpSidekickData `json:"data"`
+}
+
+type GcpSidekickData struct {
+	Credentials   GcpSidekickCredentials `json:"credentials"`
+	IDType        string                 `json:"idType"`
+	// Either the org id or project id
+	ID string `json:"id"`
+	ScanningProjectId string `json:"scanningProjectId"`
+	SharedBucket string `json:"sharedBucketName"`
+	FilterList string `json:"filterList,omitempty"`
+	QueryText string `json:"queryText,omitempty"`
+	//ScanFrequency in hours, 24 == 24 hours
+	ScanFrequency int `json:"scanFrequency"`
+	ScanContainers          bool `json:"scanContainers"`
+	ScanHostVulnerabilities bool `json:"scanHostVulnerabilities"`
+}
+
+type GcpSidekickCredentials struct {
+	ClientID     string `json:"clientId"`
+	ClientEmail  string `json:"clientEmail"`
+	PrivateKeyID string `json:"privateKeyId"`
+	PrivateKey   string `json:"privateKey,omitempty"`
+	TokenUri     string `json:"tokenUri,omitempty"`
+}
