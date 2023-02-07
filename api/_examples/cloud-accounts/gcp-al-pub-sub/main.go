f46a8c8ec140c482e8b38230581540405bd53f94
djmctavish <87976925+djmctavish@users.noreply.github.com>
2023-02-07 10:46:45 +0000

    feat(api): Addition of GcpAlPubSub integration api call (#1134)
    * feat(api): GCP Pub Sub Audit Log
    
        Addition of GcpAlPubSub integration api call

---
+++ api/_examples/cloud-accounts/gcp-al-pub-sub/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/lacework/go-sdk/api"
+)
+
+func main() {
+	lacework, err := api.NewClient(os.Getenv("LW_ACCOUNT"),
+		api.WithSubaccount(os.Getenv("LW_SUBACCOUNT")),
+		api.WithApiKeys(os.Getenv("LW_API_KEY"), os.Getenv("LW_API_SECRET")),
+		api.WithApiV2(),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res, err := lacework.V2.CloudAccounts.List()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, account := range res.Data {
+		support := "Unsupported"
+		switch account.Type {
+		case api.GcpAlPubSubCloudAccount.String():
+			support = "Supported"
+		}
+
+		// Output: INTEGRATION-GUID:INTEGRATION-TYPE:[Supported|Unsupported]
+		fmt.Printf("%s:%s:%s\n", account.IntgGuid, account.Type, support)
+	}
+
+	gcpAlPubSubData := api.GcpAlPubSubSesData{
+		Credentials: api.GcpAlPubSubCredentials{
+			ClientEmail:  "test@project.iam.gserviceaccount.com",
+			ClientID:     "0123456789",
+			PrivateKey:   "",
+			PrivateKeyID: "",
+		},
+		IntegrationType:  "PROJECT",
+		OrganizationID:   "TestOrgId",
+		ProjectID:        "TestProjectId",
+		SubscriptionName: "projects/test-project/subscriptions/test-subscription",
+		TopicID:          "projects/test-project/subscriptions/test-topic",
+	}
+
+	gcpAlPubSubCloudAccount := api.NewCloudAccount(
+		"cloud-from-golang",
+		api.GcpAlPubSubCloudAccount,
+		gcpAlPubSubData,
+	)
+
+	gcpAlPubSubResponse, err := lacework.V2.CloudAccounts.Create(gcpAlPubSubCloudAccount)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Output: GcpAlPubSub Cloud Account created: THE-INTEGRATION-GUID
+	fmt.Printf("Cloud Account created: %s", gcpAlPubSubResponse.Data.IntgGuid)
+}
