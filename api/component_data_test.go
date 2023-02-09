cae8eecbf0a7849d166a0f502173e49542db374b
Edoardo Pirovano <6748066+edoardopirovano@users.noreply.github.com>
2023-02-09 07:59:50 +0000

    feat(api): Add support for the `ComponentData` API end-points (#1135)
    * feat(RAIN-46483): Add support for the `ComponentData` API end-points
    
    * Address review comments from @afiune
    
    * Make some fixes/improvements after end-to-end testing

---
+++ api/component_data_test.go
@@ -0,0 +1,98 @@
+package api_test
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/lacework/go-sdk/api"
+	"github.com/lacework/go-sdk/internal/lacework"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUploadFiles(t *testing.T) {
+	fakeServer := lacework.MockServer()
+	fakeServer.UseApiV2()
+	fakeServer.MockToken("TOKEN")
+	defer fakeServer.Close()
+	fakeServer.MockAPI("ComponentData/requestUpload", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "POST", r.Method)
+		assert.NotNil(t, r.Body)
+		body := httpBodySniffer(r)
+		assert.Contains(t, body, "sast")
+		assert.Contains(t, body, "doc-set")
+		_, err := fmt.Fprintf(w, generateInitialResponse())
+		assert.Nil(t, err)
+	})
+	fakeServer.MockAPI("ComponentData/completeUpload", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "POST", r.Method)
+		assert.NotNil(t, r.Body)
+		body := httpBodySniffer(r)
+		assert.Contains(t, body, "SOME-GUID")
+		_, err := fmt.Fprintf(w, generateCompleteResponse())
+		assert.Nil(t, err)
+	})
+	c, err := api.NewClient("test",
+		api.WithApiV2(),
+		api.WithToken("TOKEN"),
+		api.WithURL(fakeServer.URL()),
+	)
+	assert.Nil(t, err)
+	guid, err := c.V2.ComponentData.UploadFiles("doc-set", []string{"sast"}, []string{})
+	assert.Nil(t, err)
+	assert.Equal(t, "SOME-GUID", guid)
+}
+
+func TestDoWithExponentialBackoffAlwaysFailing(t *testing.T) {
+	waited := 0
+	err := api.DoWithExponentialBackoff(func() error {
+		return errors.New("failed")
+	}, func(x int) {
+		waited += x
+	})
+	assert.NotNil(t, err)
+	assert.Equal(t, 62, waited)
+}
+
+func TestDoWithExponentialBackoffSucceedsOnThirdAttempt(t *testing.T) {
+	waited := 0
+	attempt := 1
+	err := api.DoWithExponentialBackoff(func() error {
+		if attempt == 3 {
+			return nil
+		}
+		attempt += 1
+		return errors.New("failed")
+	}, func(x int) {
+		waited += x
+	})
+	assert.Nil(t, err)
+	assert.Equal(t, 6, waited)
+}
+
+func generateInitialResponse() string {
+	return `
+{
+	"data": {
+		"guid": "SOME-GUID",
+		"uploadMethods": [
+			{
+				"method": "AwsS3",
+				"info": {}
+			}
+		]
+	}
+}
+`
+}
+
+func generateCompleteResponse() string {
+	return `
+{
+	"data": {
+		"guid": "SOME-GUID"
+	}
+}
+`
+}
