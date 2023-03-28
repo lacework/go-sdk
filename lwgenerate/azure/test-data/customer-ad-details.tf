c485751561196c8c187256d695a8d55d5cbc1811
Salim Afiune <afiune@lacework.net>
2023-03-28 13:03:52 -0700

    chore(lwgenerate): bump azure modules to 2.0 (#1210)
    Signed-off-by: Salim Afiune Maya <afiune@lacework.net>

--- lwgenerate/azure/test-data/customer-ad-details.tf
+++ lwgenerate/azure/test-data/customer-ad-details.tf
@@ -1,13 +1,5 @@
 terraform {
   required_providers {
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "~> 2.16"
-    }
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 2.91.0"
-    }
     lacework = {
       source  = "lacework/lacework"
       version = "~> 1.0"
@@ -25,7 +17,7 @@ provider "azurerm" {
 
 module "az_config" {
   source                      = "lacework/config/azure"
-  version                     = "~> 1.0"
+  version                     = "~> 2.0"
   application_id              = "AD-Test-Application-ID"
   application_password        = "AD-Test-Password"
   lacework_integration_name   = "Test Config Rename"
@@ -35,7 +27,7 @@ module "az_config" {
 
 module "az_activity_log" {
   source                      = "lacework/activity-log/azure"
-  version                     = "~> 1.0"
+  version                     = "~> 2.0"
   application_id              = "AD-Test-Application-ID"
   application_password        = "AD-Test-Password"
   lacework_integration_name   = "Test Activity Log Rename"
