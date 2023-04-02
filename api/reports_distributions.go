c64b0ed1d6453cf8504c0e3a453223301e3f88fa
Darren Murray <darren.murray@lacework.net>
2023-04-02 17:35:25 +0100

    feat(api): New Report Distributions service
    Signed-off-by: Darren Murray <darren.murray@lacework.net>

---
+++ api/reports_distributions.go
@@ -0,0 +1,160 @@
+//
+// Author:: Darren Murray (<darren.murray@lacework.net>)
+// Copyright:: Copyright 2023, Lacework Inc.
+// License:: Apache License, Version 2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package api
+
+import (
+	"fmt"
+
+	"github.com/pkg/errors"
+)
+
+// ReportDistributionsService is a service that interacts with the ReportDistributions
+// endpoints from the Lacework APIv2 Server
+type ReportDistributionsService struct {
+	client *Client
+}
+
+// The report distribution frequency type
+type reportDistributionFrequency int
+
+const (
+	ReportDistributionFrequencyDaily reportDistributionFrequency = iota
+	ReportDistributionFrequencyWeekly
+	ReportDistributionFrequencyBiweekly
+	ReportDistributionFrequencyMonthly
+)
+
+func (frequency reportDistributionFrequency) String() string {
+	return reportDistributionTypes[frequency]
+}
+
+var reportDistributionTypes = map[reportDistributionFrequency]string{
+	ReportDistributionFrequencyDaily:    "daily",
+	ReportDistributionFrequencyWeekly:   "weekly",
+	ReportDistributionFrequencyBiweekly: "biweekly",
+	ReportDistributionFrequencyMonthly:  "monthly",
+}
+
+// The report distribution violation type
+type reportDistributionViolation int
+
+const (
+	ReportDistributionViolationCompliant reportDistributionViolation = iota
+	ReportDistributionViolationNonCompliant
+	ReportDistributionViolationSuppressed
+	ReportDistributionViolationCouldNotAssess
+	ReportDistributionViolationManual
+)
+
+func (subType reportDistributionViolation) String() string {
+	return reportDistributionSubTypes[subType]
+}
+
+func ReportDistributionViolations() (values []string) {
+	for _, v := range reportDistributionSubTypes {
+		values = append(values, v)
+	}
+	return
+}
+
+var reportDistributionSubTypes = map[reportDistributionViolation]string{
+	ReportDistributionViolationCompliant:      "Compliant",
+	ReportDistributionViolationNonCompliant:   "NonCompliant",
+	ReportDistributionViolationSuppressed:     "Suppressed",
+	ReportDistributionViolationCouldNotAssess: "CouldNotAssess",
+	ReportDistributionViolationManual:         "Manual",
+}
+
+// List returns a ReportDistributionResponse
+func (svc *ReportDistributionsService) List() (response ReportDistributionsResponse, err error) {
+	err = svc.client.RequestDecoder("GET", apiV2ReportDistributions, nil, &response)
+	return
+}
+
+// Get returns a ReportDistributionResponse
+func (svc *ReportDistributionsService) Get(guid string) (response ReportDistributionResponse, err error) {
+	if guid == "" {
+		return ReportDistributionResponse{}, errors.New("specify a report distribution guid")
+	}
+	apiPath := fmt.Sprintf(apiV2ReportDistributionsFromGUID, guid)
+	err = svc.client.RequestDecoder("GET", apiPath, nil, &response)
+	return
+}
+
+// Delete a ReportDistribution
+func (svc *ReportDistributionsService) Delete(guid string) error {
+	if guid == "" {
+		return errors.New("specify a report distribution guid")
+	}
+
+	return svc.client.RequestDecoder("DELETE", fmt.Sprintf(apiV2ReportDistributionsFromGUID, guid), nil, nil)
+}
+
+func (svc *ReportDistributionsService) Create(report ReportDistribution) (response ReportDistributionResponse, err error) {
+	err = svc.client.RequestEncoderDecoder("POST", apiV2ReportDistributions, report, &response)
+	return
+}
+
+func (svc *ReportDistributionsService) Update(guid string, report ReportDistributionConfig) (response ReportDistributionResponse, err error) {
+	if guid == "" {
+		return response, errors.New("specify a report distribution guid")
+	}
+
+	err = svc.client.RequestEncoderDecoder("PATCH", fmt.Sprintf(apiV2ReportDistributionsFromGUID, guid), report, &response)
+	return
+}
+
+type ReportDistributionsResponse struct {
+	Data []ReportDistribution `json:"data"`
+}
+
+type ReportDistributionResponse struct {
+	Data ReportDistribution `json:"data"`
+}
+
+type ReportDistribution struct {
+	ReportDistributionGuid string   `json:"guid,omitempty" yaml:"guid,omitempty"`
+	ReportDefinitionGuid   string   `json:"reportDefinitionGuid"`
+	ResourceGroups         []string `json:"resourceGroups"`
+	Integrations           []struct {
+		AccountId string `json:"accountId"`
+	} `json:"integrations"`
+	AlertChannels []string `json:"alertChannels"`
+	Frequency     string   `json:"frequency"`
+	Severities    []string `json:"severities"`
+	Violations    []string `json:"violations"`
+}
+
+type ReportDistributionConfig struct {
+	ResourceGroups       []string                    `json:"resourceGroups"`
+	Integrations         ReportDefinitionIntegration `json:"integrations"`
+	AlertChannels        []string                    `json:"alertChannels"`
+	Frequency            string                      `json:"frequency"`
+	Severities           []string                    `json:"severities"`
+	Violations           []string                    `json:"violations"`
+	ReportDefinitionGuid string                      `json:"reportDefinitionGuid"`
+}
+
+type ReportDefinitionIntegration struct {
+	AccountId      string `json:"accountId"`
+	OrganizationId string `json:"organizationId"`
+	ProjectId      string `json:"projectId"`
+	TenantId       string `json:"tenantId"`
+	SubscriptionId string `json:"subscriptionId"`
+}
