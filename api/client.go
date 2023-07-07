46711246b7da02c1181e433a0f613448c66cda1e
Kolbeinn <kolbeinn.karlsson@lacework.net>
2023-07-07 14:24:08 +0000

    fix(api): fix overly narrow type in withTransport argument (#1314)
    Signed-off-by: Kolbeinn Karlsson <kolbeinn.karlsson@lacework.net>

--- api/client.go
+++ api/client.go
@@ -203,7 +203,7 @@ func WithRetries(retries *backoff.ExponentialBackOff) Option {
 }
 
 // WithTransport changes the default transport to increase TLSHandshakeTimeout
-func WithTransport(transport *http.Transport) Option {
+func WithTransport(transport http.RoundTripper) Option {
 	return clientFunc(func(c *Client) error {
 		c.c.Transport = transport
 		return nil
