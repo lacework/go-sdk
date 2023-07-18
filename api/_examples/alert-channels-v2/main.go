4bf154e2176eb547177b307694681dbdee9e5066
Salim Afiune Maya <afiune@lacework.net>
2023-07-17 18:13:15 -0700

    docs(api): Active containers example
    Closes https://github.com/lacework/go-sdk/issues/1236
    
    Signed-off-by: Salim Afiune Maya <afiune@lacework.net>

--- api/_examples/alert-channels-v2/main.go
+++ api/_examples/alert-channels-v2/main.go
@@ -10,6 +10,7 @@ import (
 
 func main() {
 	lacework, err := api.NewClient(os.Getenv("LW_ACCOUNT"),
+		api.WithSubaccount(os.Getenv("LW_SUBACCOUNT")),
 		api.WithApiKeys(os.Getenv("LW_API_KEY"), os.Getenv("LW_API_SECRET")))
 	if err != nil {
 		log.Fatal(err)
