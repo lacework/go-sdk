1553c2a70ab79b1507ad9eb53892d68fb4c8252a
Pengyuan Zhao <pengyuan.zhao@lacework.net>
2023-09-13 14:03:47 -0400

    fix(GROW-2435): change alert source Gcp to GCP

--- api/alert_rules.go
+++ api/alert_rules.go
@@ -32,7 +32,7 @@ type AlertRulesService struct {
 }
 
 // Valid inputs for AlertRule Source property
-var AlertRuleSources = []string{"Agent", "AWS", "Azure", "Gcp", "K8s"}
+var AlertRuleSources = []string{"Agent", "AWS", "Azure", "GCP", "K8s"}
 
 // Valid inputs for AlertRule Categories property
 var AlertRuleCategories = []string{"Anomaly", "Policy", "Composite"}
