78f107cb48b829b7427c3a12dab2b7f0a91ca5ff
Darren Murray <darren.murray@lacework.net>
2023-09-18 16:29:04 +0100

    test: remove t.skip for cloud_account tests
    Signed-off-by: Darren Murray <darren.murray@lacework.net>

--- integration/cloud_account_test.go
+++ integration/cloud_account_test.go
@@ -26,7 +26,6 @@ import (
 )
 
 func TestCloudAccountCommandAliases(t *testing.T) {
-	t.Skip()
 	// lacework cloud-account
 	out, err, exitcode := LaceworkCLI("help", "cloud-account")
 	assert.Contains(t, out.String(), "lacework cloud-account [command]")
@@ -53,7 +52,6 @@ func TestCloudAccountCommandAliases(t *testing.T) {
 }
 
 func _TestCloudAccountCommandList(t *testing.T) {
-	t.Skip()
 	out, err, exitcode := LaceworkCLIWithTOMLConfig("cloud-account", "list")
 	assert.Contains(t, out.String(), "CLOUD ACCOUNT GUID",
 		"STDOUT table headers changed, please check")
@@ -73,7 +71,6 @@ func _TestCloudAccountCommandList(t *testing.T) {
 }
 
 func _TestCloudAccountCommandListWithTypeFlag(t *testing.T) {
-	t.Skip()
 	out, err, exitcode := LaceworkCLIWithTOMLConfig("cloud-account", "list", "--type", "AWS_CFG")
 	assert.Contains(t, out.String(), "CLOUD ACCOUNT GUID",
 		"STDOUT table headers changed, please check")
@@ -95,7 +92,6 @@ func _TestCloudAccountCommandListWithTypeFlag(t *testing.T) {
 }
 
 func _TestCloudAccountCommandListWithTypeFlagErrorUnknownType(t *testing.T) {
-	t.Skip()
 	out, err, exitcode := LaceworkCLIWithTOMLConfig("cloud-account", "list", "--type", "FOO_BAR")
 	assert.Emptyf(t, out.String(),
 		"STDOUT should be empty")
@@ -107,7 +103,6 @@ func _TestCloudAccountCommandListWithTypeFlagErrorUnknownType(t *testing.T) {
 }
 
 func TestCloudAccountShow(t *testing.T) {
-	t.Skip()
 	out, err, exitcode := LaceworkCLIWithTOMLConfig("cloud-account", "show", "TECHALLY_948AB76C4F809D5CBE4C92BB38F6EBFD9F413694FD85C75")
 	// Summary Table
 	assert.Contains(t, out.String(), "CLOUD ACCOUNT GUID",
@@ -149,7 +144,6 @@ func TestCloudAccountShow(t *testing.T) {
 }
 
 func TestCloudAccountList(t *testing.T) {
-	t.Skip()
 	out, err, exitcode := LaceworkCLIWithTOMLConfig("ca", "list")
 	assert.Contains(t, out.String(), "CLOUD ACCOUNT GUID",
 		"STDOUT table headers changed, please check")
