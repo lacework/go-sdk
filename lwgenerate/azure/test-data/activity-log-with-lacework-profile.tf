0e783aa0d147b49c9d5abf8f8e37ba2474f6c3a7
ipcrm <matthew.cadorette@lacework.net>
2023-11-02 13:51:44 -0400

    fix(GROW-2556): enable azure storage account infra encryption
    Enable Infrastructure Encryption by default when creating a new Azure
    Storage Account for activity log integration.

--- lwgenerate/azure/test-data/activity-log-with-lacework-profile.tf
+++ lwgenerate/azure/test-data/activity-log-with-lacework-profile.tf
@@ -25,10 +25,11 @@ module "az_ad_application" {
 }
 
 module "az_activity_log" {
-  source                      = "lacework/activity-log/azure"
-  version                     = "~> 2.0"
-  application_id              = module.az_ad_application.application_id
-  application_password        = module.az_ad_application.application_password
-  service_principal_id        = module.az_ad_application.service_principal_id
-  use_existing_ad_application = true
+  source                            = "lacework/activity-log/azure"
+  version                           = "~> 2.0"
+  application_id                    = module.az_ad_application.application_id
+  application_password              = module.az_ad_application.application_password
+  infrastructure_encryption_enabled = true
+  service_principal_id              = module.az_ad_application.service_principal_id
+  use_existing_ad_application       = true
 }
