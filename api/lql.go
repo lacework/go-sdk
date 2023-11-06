21a27db7ae64653642097b7643641b494f0ea6a1
Xiao Fu <xiao.fu@lacework.net>
2023-11-06 13:55:35 -0500

    feat(RAIN-74824): support queryLanguage when creating query

--- api/lql.go
+++ api/lql.go
@@ -29,8 +29,9 @@ import (
 )
 
 type NewQuery struct {
-	QueryID   string `json:"queryId" yaml:"queryId"`
-	QueryText string `json:"queryText" yaml:"queryText"`
+	QueryID       string `json:"queryId" yaml:"queryId"`
+	QueryLanguage string `json:"queryLanguage" yaml:"queryLanguage"`
+	QueryText     string `json:"queryText" yaml:"queryText"`
 }
 
 func ParseNewQuery(s string) (NewQuery, error) {
