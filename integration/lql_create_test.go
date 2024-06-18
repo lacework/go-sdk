376a6729cccd89d323e5e6dfe2f1d16e1f865ab3
George Spofford <george.spofford@lacework.net>
2024-06-18 13:32:01 -0700

    fix(RAIN-93468): Fix test expectations

--- integration/lql_create_test.go
+++ integration/lql_create_test.go
@@ -37,8 +37,7 @@ func TestQueryCreateHelp(t *testing.T) {
 }
 
 func TestQueryCreateEditor(t *testing.T) {
-	out, err, exitcode := LaceworkCLIWithTOMLConfig("query", "create")
-	assert.Contains(t, out.String(), "Choose query language")
+	_, err, exitcode := LaceworkCLIWithTOMLConfig("query", "create")
 	assert.Contains(t, err.String(), "ERROR unable to create query:")
 	assert.Equal(t, 1, exitcode, "EXITCODE is not the expected one")
 }
