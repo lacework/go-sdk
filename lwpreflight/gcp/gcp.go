3079d6ccc618f1b18e10c05bcd6c51e722d22d75
Pengyuan Zhao <pengyuan.zhao@lacework.net>
2025-06-03 11:01:16 -0400

    feat(preflight): build preflight package for GCP

---
+++ lwpreflight/gcp/gcp.go
@@ -0,0 +1,106 @@
+package gcp
+
+import (
+	"errors"
+	"os"
+
+	"golang.org/x/oauth2"
+	"google.golang.org/api/option"
+)
+
+type Preflight struct {
+	gcpClientOption  option.ClientOption
+	projectID        string
+	orgID            string
+	integrationTypes []IntegrationType
+	permissions      map[string]bool
+	tasks            []func(p *Preflight) error
+
+	caller  Caller
+	details Details
+	errors  map[IntegrationType][]string
+}
+
+type Result struct {
+	Caller  Caller
+	Details Details
+	Errors  map[IntegrationType][]string
+}
+
+type Params struct {
+	Agentless       bool
+	AuditLog        bool
+	Config          bool
+	Region          string
+	OrgID           string // Org-level integration if non-empty
+	ProjectID       string
+	AccessToken     string
+	CredentialsFile string // Path to the credential JSON file
+}
+
+func New(params Params) (*Preflight, error) {
+	integrationTypes := []IntegrationType{}
+	tasks := []func(p *Preflight) error{
+		FetchCaller,
+		FetchPolicies,
+		CheckPermissions,
+		FetchDetails,
+	}
+
+	if params.Agentless {
+		integrationTypes = append(integrationTypes, Agentless)
+	}
+	if params.AuditLog {
+		integrationTypes = append(integrationTypes, AuditLog)
+	}
+	if params.Config {
+		integrationTypes = append(integrationTypes, Config)
+	}
+
+	if params.ProjectID == "" {
+		return nil, errors.New("ProjectID must be provided")
+	}
+
+	preflight := &Preflight{
+		projectID:        params.ProjectID,
+		orgID:            params.OrgID,
+		integrationTypes: integrationTypes,
+		permissions:      map[string]bool{},
+		tasks:            tasks,
+		details:          Details{},
+		errors:           map[IntegrationType][]string{},
+	}
+
+	if params.AccessToken != "" {
+		token := &oauth2.Token{AccessToken: params.AccessToken}
+		preflight.gcpClientOption = option.WithTokenSource(oauth2.StaticTokenSource(token))
+	} else {
+		var credentialsFile string
+		if params.CredentialsFile != "" {
+			credentialsFile = params.CredentialsFile
+		} else if os.Getenv("GOOGLE_APPLICATION_CREDENTIALS") != "" {
+			credentialsFile = os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
+		}
+		if credentialsFile == "" {
+			return nil, errors.New("CredentialsFile or AccessToken must be provided. Alternatively, set the GOOGLE_APPLICATION_CREDENTIALS environment variable.")
+		}
+		preflight.gcpClientOption = option.WithCredentialsFile(credentialsFile)
+	}
+
+	return preflight, nil
+}
+
+func (p *Preflight) Run() (*Result, error) {
+	for _, task := range p.tasks {
+		err := task(p)
+		if err != nil {
+			return nil, err
+		}
+	}
+	result := &Result{
+		Caller:  p.caller,
+		Details: p.details,
+		Errors:  p.errors,
+	}
+	return result, nil
+}
